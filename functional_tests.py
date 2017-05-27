39fec8f3540e3b3486c26f6b61e880806155abf9
Harald Ellingsen <haraldellingsen@harald.local>
2017-05-27 09:45:02 +0200

    Refactor with helper method to chech for list items

--- functional_tests.py
+++ functional_tests.py
@@ -10,6 +10,11 @@ class newVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+    	table = self.browser.find_element_by_id('id_list_table')
+    	rows = table.find_elements_by_tag_name('tr')
+    	self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Trude har på jobben hørt snakk om en ny fet todo liste webapp 
         # åpner nettleseren for å sjekke den ut
@@ -33,10 +38,7 @@ class newVisitorTest(unittest.TestCase):
         # "1    Kjøp maggott til Bjerne"
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Kjøp maggot', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Kjøp maggot')
 
         # Ennå en tekstboks inviterer til å legge til enda en oppføring. Hun 
         # skriver inn "Gi maggott til Bjerne ved neste anledning" og trykker enter.
@@ -46,10 +48,8 @@ class newVisitorTest(unittest.TestCase):
         time.sleep(1)
 
         # Siden oppdateres igjen og begge oppføringene vises på lista.
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Kjøp maggot', [row.text for row in rows])
-        self.assertIn('2: Gi maggott til Bjerne', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Kjøp maggot')
+        self.check_for_row_in_list_table('2: Gi maggott til Bjerne')
 
         # Trude lurer spent på om denne siden kommer til å huske hennes liste når hun lukker ned.
         # Hun er observant og legger merke til at siden har genrert en unik URL for henne --
