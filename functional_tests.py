0b2e291887f1521e938014938340b1338ef7b15c
Harald Ellingsen <haraldellingsen@harald.local>
2017-05-27 09:38:52 +0200

    Intermediate commit before refactoring. Code handles only one item.

--- functional_tests.py
+++ functional_tests.py
@@ -27,7 +27,7 @@ class newVisitorTest(unittest.TestCase):
             'Enter a to-do item'
         )
         # Hun skriver inn "Kjøp maggott til Bjerne" i en tekstboks
-        inputbox.send_keys('Kjøp maggott til Bjerne')
+        inputbox.send_keys('Kjøp maggot')
 
         # Når hun trykker enter så oppdateres siden og første oppføring vises:
         # "1    Kjøp maggott til Bjerne"
@@ -36,18 +36,25 @@ class newVisitorTest(unittest.TestCase):
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Kjøp maggott til Bjerne' for row in rows), "New to-do item did not appear in table"
-        )
+        self.assertIn('1: Kjøp maggot', [row.text for row in rows])
 
         # Ennå en tekstboks inviterer til å legge til enda en oppføring. Hun 
         # skriver inn "Gi maggott til Bjerne ved neste anledning" og trykker enter.
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Gi maggot til Bjerne')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
         # Siden oppdateres igjen og begge oppføringene vises på lista.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Kjøp maggot', [row.text for row in rows])
+        self.assertIn('2: Gi maggott til Bjerne', [row.text for row in rows])
 
         # Trude lurer spent på om denne siden kommer til å huske hennes liste når hun lukker ned.
         # Hun er observant og legger merke til at siden har genrert en unik URL for henne --
         # det vises også noe forklarende tekst i forbindelse med lagring og den unike URL
+        self.fail('Finish the test!')
 
         # Hun besøker denne unike URLen - hennes to-do list er der stadig vekk
 
