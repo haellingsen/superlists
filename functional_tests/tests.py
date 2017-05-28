e7ae8dafcbf2fddd501bb602c55f7bfc0df2bf76
Harald Ellingsen <haraldellingsen@harald.local>
2017-05-28 16:03:51 +0200

    first steps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -113,3 +113,27 @@ class newVisitorTest(LiveServerTestCase):
         self.assertIn('Kjøp melk', page_text)
 
         # Begge fornøyde vender tilbake til søvnriket
+
+    def test_layout_and_styling(self):
+        # Trude besøker siten
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Hun legger merke til at input boxen er pent sentrert på siden
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 
+            512,
+            delta=10
+        )
+
+        # Hun starter en ny liste og ser at også her er input boxen pent sentrert
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
