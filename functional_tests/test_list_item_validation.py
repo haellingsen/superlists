3b6572334d1fa90c1123578223bfc38f469fc807
Harald Ellingsen <haraldellingsen@harald.local>
2017-06-08 20:50:58 +0200

    Implement duplicate item validation at model layer

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -2,7 +2,7 @@ from selenium.webdriver.common.keys import Keys
 from unittest import skip
 from .base import FunctionalTest
 
-class ItemValididtyTest(FunctionalTest):
+class ItemValidityTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
         # Trude prøver ved en feil å sende inn en tom tekst streng. Hun trykker enter
@@ -42,3 +42,20 @@ class ItemValididtyTest(FunctionalTest):
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Kjøp melk')
         self.wait_for_row_in_list_table('2: Lag te')
+
+    def test_cannot_add_duplicate_items(self):
+        # Trude åpner hjemmesida og starter ei ny liste
+        self.browser.get(self.live_server_url)
+        self.get_item_input_box().send_keys('Kjøp fisk')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Kjøp fisk')
+
+        # Tilfeldigvis prøver hun å legge inn samme tekst igjen
+        self.get_item_input_box().send_keys('Kjøp fisk')
+        self.get_item_input_box().send_keys(Keys.ENTER)
+        
+        # Hun ser en hjelpsom feilmelding
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element_by_css_selector('.has-error').text, 
+            "You've already got this in your list"
+        ))
