1bc753d1cc24308e92ee8f0e9a32b875a528d3ec
Harald Ellingsen <haraldellingsen@harald.local>
2017-07-03 22:39:02 +0200

    New FT validation error message display and hide test

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -3,7 +3,10 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
-from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+from lists.forms import (
+	EMPTY_ITEM_ERROR, DUPLICATE_ITEM_ERROR,
+	ExistingListItemForm, ItemForm,
+)
 from lists.views import home_page
 from lists.models import Item, List
 from unittest import skip
@@ -39,6 +42,12 @@ class ListViewTest(TestCase):
 		self.assertContains(response, 'itemey 2')
 		self.assertNotContains(response, 'other list item 1')
 		self.assertNotContains(response, 'other list item 2')
+
+	def test_displays_item_form(self):
+		list_ = List.objects.create()
+		response = self.client.get(f'/lists/{list_.id}/')
+		self.assertIsInstance(response.context['form'], ExistingListItemForm)
+		self.assertContains(response, 'name="text"')
 
 	def test_passes_correct_list_to_template(self):
 		other_list =  List.objects.create()
@@ -94,13 +103,12 @@ class ListViewTest(TestCase):
 
 	def test_for_invalid_input_passes_form_to_template(self):
 		response = self.post_invalid_input()
-		self.assertIsInstance(response.context['form'], ItemForm)
+		self.assertIsInstance(response.context['form'], ExistingListItemForm)
 
 	def test_for_invalid_input_shows_error_on_page(self):
 		response = self.post_invalid_input()
 		self.assertContains(response, escape(EMPTY_ITEM_ERROR))
 
-	@skip
 	def test_duplicate_item_validation_errors_end_up_on_lists_page(self):
 		list1 = List.objects.create()
 		item1 = Item.objects.create(list=list1, text='textey')
@@ -109,16 +117,11 @@ class ListViewTest(TestCase):
 			data={'text': 'textey'}
 		)
 
-		expected_error = escape("You've already got this in your list")
+		expected_error = escape(DUPLICATE_ITEM_ERROR)
 		self.assertContains(response, expected_error)
 		self.assertTemplateUsed(response, 'list.html')
 		self.assertEqual(Item.objects.all().count(), 1)
 
-	def test_displays_item_form(self):
-		list_ = List.objects.create()
-		response = self.client.get(f'/lists/{list_.id}/')
-		self.assertIsInstance(response.context['form'], ItemForm)
-		self.assertContains(response, 'name="text"')
 
 class NewListTest(TestCase):
 
@@ -127,17 +130,21 @@ class NewListTest(TestCase):
 		self.assertEqual(Item.objects.count(), 1)
 		new_item = Item.objects.first()
 		self.assertEqual(new_item.text, 'A new list item')
+
 	def test_redirects_after_POST(self):
 		response = self.client.post('/lists/new', data={'text': 'A new list item'})
 		new_list = List.objects.first()
 		self.assertRedirects(response, f'/lists/{new_list.id}/')
+	
 	def test_for_invalid_input_renders_home_template(self):
 		response = self.client.post('/lists/new', data={'text': ''})
 		self.assertEqual(response.status_code, 200)
 		self.assertTemplateUsed(response, 'home.html')
+	
 	def test_validation_errors_are_shown_on_home_page(self):
 		response = self.client.post('/lists/new', data={'text': ''})
 		self.assertContains(response, escape(EMPTY_ITEM_ERROR))
+
 	def test_for_invalid_input_passes_form_to_template(self):
 		response = self.client.post('/lists/new', data={'text': ''})
 		self.assertIsInstance(response.context['form'], ItemForm)
