f1881657ae297529a0ed7045cfbf08b044cca33a
Harald Ellingsen <haraldellingsen@harald.local>
2017-07-13 22:13:25 +0200

    added favicon in urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls import url
 from accounts import views
 from django.contrib.auth.views import logout
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+	url(r'^favicon.ico$', 
+		RedirectView.as_view(
+			url=staticfiles_storage.url('favicon.ico'), 
+			permanent=False), 
+		name="favicon"),
 	url(r'^send_login_email$', views.send_login_email, name='send_login_email'),
 	url(r'^login$', views.login, name='login'),
 	url(r'^logout$', logout, {'next_page': '/'}, name='logout'),
