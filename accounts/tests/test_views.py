d86dcc7909c814b7708b8b60f3626b85f40fff1f
Harald Ellingsen <haraldellingsen@harald.local>
2017-08-11 03:11:22 +0200

    expected_url s/%d/%s

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -49,7 +49,7 @@ class SendLoginEmailViewTest(TestCase):
 		})
 
 		token = Token.objects.first()
-		expected_url = 'http://testserver/accounts/login?token=%d' % (token.uid,)
+		expected_url = 'http://testserver/accounts/login?token=%s' % (token.uid,)
 		(subject, body, from_email, to_list), kwargs = mock_send_mail.call_args
 		self.assertIn(expected_url, body)
 
