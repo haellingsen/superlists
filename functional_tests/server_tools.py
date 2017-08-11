4b12184d5486be54955dc821c92efa83cfc1531b
Harald Ellingsen <haraldellingsen@harald.local>
2017-08-11 03:37:54 +0200

    s/f''/'%[sd]' % (var,)/

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -2,15 +2,15 @@ from fabric.api import run
 from fabric.context_managers import settings
 
 def _get_manage_dot_py(host):
-	return f'~/sites/{host}/virtualenv/bin/python ~/sites/{host}/source/manage.py'
+	return '~/sites/%s/virtualenv/bin/python ~/sites/%s/source/manage.py' % (host,)
 
 def reset_database(host):
 	manage_dot_py = _get_manage_dot_py(host)
-	with settings(host_string=f'ubuntu@{host}'):
-		run(f'{manage_dot_py} flush --noinput')
+	with settings(host_string='ubuntu@%s' % (host,)):
+		run('%s flush --noinput' % (manage_dot_py,))
 
 def create_session_on_server(host, email):
 	manage_dot_py = _get_manage_dot_py(host)
-	with settings(host_string=f'ubuntu@{host}'):
-		session_key = run(f'{manage_dot_py} create_session {email}')
+	with settings(host_string='ubuntu@%s' % (host,)):
+		session_key = run('%s create_session %s' % (manage_dot_py,email,))
 		return session_key.strip()
