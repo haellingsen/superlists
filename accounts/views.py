e7c0dd6635efef0baeaab6422fb4c27032550f67
Harald Ellingsen <haraldellingsen@harald.local>
2017-08-11 02:47:04 +0200

    removed f in front of single quote on line 13

--- accounts/views.py
+++ accounts/views.py
@@ -10,7 +10,7 @@ def send_login_email(request):
 	url = request.build_absolute_uri(
 		reverse('login') + '?token=' + str(token.uid)
 	)
-	message_body = f'Use this link to log in:\n\n{url}'.format(url=url)
+	message_body = 'Use this link to log in:\n\n{url}'.format(url=url)
 	send_mail(
 		'Your login link for todo lists',
 		message_body,
