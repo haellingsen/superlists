e4def4306a15b6649ab63023065603f3b144d37b
Harald Ellingsen <haraldellingsen@harald.local>
2017-08-11 04:29:08 +0200

    screenshotting and dumping html from jenkins CI server

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,5 +1,6 @@
 import os
 import time
+from datetime import datetime
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
@@ -7,61 +8,98 @@ from selenium.webdriver.common.keys import Keys
 from .server_tools import reset_database
 
 MAX_WAIT = 10
+SCREEN_DUMP_LOCATION = os.path.join(
+	os.path.dirname(os.path.abspath(__file__)), 'screendumps'
+)
 
 def wait(fn):
-    def modified_fn(*args, **kwargs):  
-        start_time = time.time()
-        while True:
-            try:
-                return fn(*args, **kwargs)  
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-    return modified_fn
+	def modified_fn(*args, **kwargs):  
+		start_time = time.time()
+		while True:
+			try:
+				return fn(*args, **kwargs)  
+			except (AssertionError, WebDriverException) as e:
+				if time.time() - start_time > MAX_WAIT:
+					raise e
+				time.sleep(0.5)
+	return modified_fn
 
 class FunctionalTest(StaticLiveServerTestCase):
 
-    def setUp(self):
-        self.browser = webdriver.Firefox()
-        self.staging_server = os.environ.get('STAGING_SERVER')
-        if self.staging_server:
-            self.live_server_url = 'http://' + self.staging_server
-            reset_database(self.staging_server)
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.staging_server = os.environ.get('STAGING_SERVER')
+		if self.staging_server:
+			self.live_server_url = 'http://' + self.staging_server
+			reset_database(self.staging_server)
 
-    def tearDown(self):
-        self.browser.quit()
+	def tearDown(self):
+		if self._test_has_failed():
+			if not os.path.exists(SCREEN_DUMP_LOCATION):
+				os.makedirs(SCREEN_DUMP_LOCATION)
+			for ix, handle in enumerate(self.browser.window_handles):
+				self._windowid = ix
+				self.browser.switch_to_window(handle)
+				self.take_screenshot()
+				self.dump_html()
+		self.browser.quit()
+		super().tearDown()
 
-    @wait
-    def wait_for_row_in_list_table(self, row_text):
-       	table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+	def _test_has_failed(self):
+		# Slightly obscure but couldn't find a better way!
+		return any(error for (method, error) in self._outcome.errors)
 
-    @wait
-    def wait_for(self, fn):
-        return fn()
+	def take_screenshot(self):
+		filename = self._get_filename() + '.png'
+		print('screenshotting to', filename)
+		self.browser.get_screenshot_as_file(filename)
 
-    def get_item_input_box(self):
-        return self.browser.find_element_by_id('id_text')
+	def dump_html(self):
+		filename = self._get_filename() + '.html'
+		print('dumping page HTML to' + filename)
+		with open(filename, 'w') as f:
+			f.write(self.browser.page_source)
 
-    @wait
-    def wait_to_be_logged_in(self, email):
-        self.browser.find_element_by_link_text('Log out')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertIn(email, navbar.text)
+	def _get_filename(self):
+		timestamp = datetime.now().isoformat().replace(':', '.')[:19]
+		return '{folder}/{classname}.{method}-window{windowid}-{timestamp}'.format(
+			folder=SCREEN_DUMP_LOCATION,
+			classname=self.__class__.__name__,
+			method=self._testMethodName,
+			windowid=self._windowid,
+			timestamp=timestamp
+		)
+		
+	@wait
+	def wait_for_row_in_list_table(self, row_text):
+		table = self.browser.find_element_by_id('id_list_table')
+		rows = table.find_elements_by_tag_name('tr')
+		self.assertIn(row_text, [row.text for row in rows])
 
-    @wait
-    def wait_to_be_logged_out(self, email):
-        self.browser.find_element_by_name('email')
-        navbar = self.browser.find_element_by_css_selector('.navbar')
-        self.assertNotIn(email, navbar.text)
+	@wait
+	def wait_for(self, fn):
+		return fn()
 
-    def add_list_item(self, item_text):
-        num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
-        self.get_item_input_box().send_keys(item_text)
-        self.get_item_input_box().send_keys(Keys.ENTER)
-        item_number = num_rows + 1
-        self.wait_for_row_in_list_table(
-            '{}: {}'.format(item_number, item_text)
-        )
+	def get_item_input_box(self):
+		return self.browser.find_element_by_id('id_text')
+
+	@wait
+	def wait_to_be_logged_in(self, email):
+		self.browser.find_element_by_link_text('Log out')
+		navbar = self.browser.find_element_by_css_selector('.navbar')
+		self.assertIn(email, navbar.text)
+
+	@wait
+	def wait_to_be_logged_out(self, email):
+		self.browser.find_element_by_name('email')
+		navbar = self.browser.find_element_by_css_selector('.navbar')
+		self.assertNotIn(email, navbar.text)
+
+	def add_list_item(self, item_text):
+		num_rows = len(self.browser.find_elements_by_css_selector('#id_list_table tr'))
+		self.get_item_input_box().send_keys(item_text)
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		item_number = num_rows + 1
+		self.wait_for_row_in_list_table(
+			'{}: {}'.format(item_number, item_text)
+		)
